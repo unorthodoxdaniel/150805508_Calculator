6aa8c9c1c971d6ff66332e5e358fa6164723d30c
unorthodoxdaniel <unorthodoxdaniel@gmail.com>
2018-02-17 02:56:33 +0100

    Planning
    Planning stage for programming calculator functions and computations.

---
+++ calculator-brain.js
@@ -0,0 +1,73 @@
+//FIND 306 note. This may be an implementation of parse tree
+
+//Create a sting called computation
+//For every button clicked. the inner HTML AND the string is updated
+//Once the user types equal, then te traverse string operation starts.
+//It goes through a value, breaks at a sign and then stores the value as a number
+//Once it reaches a sign, it takes note and continues the getting number function until the end of the string
+//Decimal doesn't need a function because the traverse should automatically convert the string into a floating point integer (this is javascript)
+
+var computation;
+
+function add (a, b){
+    return a + b;
+}
+
+function subtract (){
+    return a - b;
+
+}
+
+function multiply (){
+    return a * b;
+}
+
+function divide (){
+    return a / b;
+}
+
+function sin (){
+    
+
+}
+
+function cos (){
+
+}
+
+function tan (){
+
+}
+
+function mod (){
+    return a % b;
+}
+
+function exp (){
+
+}
+
+function log (){
+
+}
+
+function factorial (){
+
+}
+
+function clear (){
+    document.getElementById("result").innerHTML = ""; //To clear the "screen"
+    //Afunction needs to be here to clear the simulated memory
+}
+
+function equals (){
+
+}
+
+function traverseString(){
+
+}
+
+/*function BodmasChecker(){
+
+}*/
